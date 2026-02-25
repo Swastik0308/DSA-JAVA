8b31615f75e6d5095fb6e4e11d1780ff288cfe49
Swastik0308 <swastikshetty253@gmail.com>
2026-02-25 19:52:16 +0530

    Few updation

--- Hashing/HashMapIntro.java
+++ Hashing/HashMapIntro.java
@@ -1,4 +1,4 @@
-import java.util.HashMap;
+import java.util.*;
 
 public class HashMapIntro {
     public static void main(String[] args) {
@@ -17,19 +17,27 @@ public class HashMapIntro {
         System.out.println(hm.get("Tea")); // if key doesnt exist it returns null
 
         // Contains Key - O(1)
-        System.out.println(hm.containsKey("Tea"));
+        System.out.println(hm.containsKey("tea"));
         System.out.println(hm.containsKey("Samosa"));// false
 
         // Remove - O(1)
-        hm.remove("Rice");
-        System.out.println(hm);
+        // hm.remove("Rice");
+        // System.out.println(hm);
 
         // Size
         System.out.println(hm.size());
 
         // IsEmpty
-        hm.clear(); // It will clear the hashmap
-        System.out.println(hm.isEmpty());
-        System.out.println(hm);
+        // hm.clear(); // It will clear the hashmap
+        // System.out.println(hm.isEmpty());
+        // System.out.println(hm);
+
+        // Iterate
+        Set<String> keys = hm.keySet();
+        // System.out.println(keys);
+
+        for (String k : keys) {
+            System.out.println("Key= " + k + " ,value = " + hm.get(k));
+        }
     }
 }
